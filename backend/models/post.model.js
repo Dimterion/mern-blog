1fd23cfdb063ec0784c19137a5f92cc88f831afb
Dmitrii <dimterion59@gmail.com>
2024-02-26 11:07:41 +0100

    add create post api route

---
+++ backend/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://raw.githubusercontent.com/Dimterion/Notes-app/master/frontend/src/assets/home_page_img.jpg",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
