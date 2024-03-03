4958f573be202d41c60e1413623b019f89f102a2
Dmitrii <dimterion59@gmail.com>
2024-03-03 19:39:06 +0100

    add about & projects pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,3 +1,18 @@
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Dimterion&apos;s Blog
+          </h1>
+          <div className="text-gray-500 flex flex-col gap-6">
+            <p>Information about the blog.</p>
+            <p>Additional information about the blog.</p>
+            <p>More information about the blog.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,3 +1,11 @@
+import CallToAction from "../components/CallToAction";
+
 export default function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-gray-500">Projects page description.</p>
+      <CallToAction />
+    </div>
+  );
 }
