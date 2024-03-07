d42f5166aec92de9e430eda299d838abf003be3f
Dmitrii <dimterion59@gmail.com>
2024-03-07 11:41:53 +0100

    change header structure

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa6";
@@ -61,35 +61,40 @@ export default function Header() {
         to="/"
         className="self-center whitespace-nowrap sm:text-xl font-semibold dark:text-white"
       >
-        <span className="p-1 bg-gradient-to-r from-blue-500 via-sky-500 to-cyan-500 rounded-lg text-white">
+        <span className="p-1 bg-gradient-to-r from-cyan-500 via-sky-500 to-blue-500 rounded-lg text-white">
           Dimterion&apos;s
         </span>
         <span>site</span>
       </Link>
-      <form onSubmit={handleSubmit}>
-        <TextInput
+      <form onSubmit={handleSubmit} className="hidden sm:flex">
+        <input
           type="text"
           placeholder="Search..."
-          rightIcon={AiOutlineSearch}
-          className="hidden sm:inline"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          className="p-2 bg-gray-50 border-gray-300 border-r-0 rounded-l-md focus:border-sky-500 dark:focus:border-sky-500 focus:ring-0 dark:bg-gray-700 dark:border-gray-600"
         />
+        <button
+          type="submit"
+          className="bg-sky-500 hover:bg-sky-400 text-white px-4 py-2 rounded-r-md"
+        >
+          <AiOutlineSearch />
+        </button>
       </form>
-      <Button
-        className="size-7 md:size-10 sm:hidden"
-        color="gray"
-        onClick={handleSubmit}
-      >
-        <AiOutlineSearch />
-      </Button>
       <div className="flex gap-1 md:order-2 items-center">
+        <Button
+          className="size-7 md:size-10 sm:hidden"
+          color="gray"
+          onClick={handleSubmit}
+        >
+          <AiOutlineSearch />
+        </Button>
         <Button
           className="size-7 md:size-10"
           color="gray"
           onClick={() => dispatch(toggleTheme())}
         >
-          {theme === "light" ? <FaSun /> : <FaMoon />}
+          {theme === "light" ? <FaMoon /> : <FaSun />}
         </Button>
         {currentUser ? (
           <Dropdown
