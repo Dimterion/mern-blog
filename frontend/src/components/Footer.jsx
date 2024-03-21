cd6a2ad746516cba9824469ebe4ef61069928df9
Dmitrii <dimterion59@gmail.com>
2024-03-21 11:55:12 +0100

    adjust footer comp

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -12,72 +12,41 @@ export default function FooterComponent() {
   return (
     <Footer container className="border-t-2 border-sky-500 rounded-none">
       <section className="w-full max-w-7xl mx-auto">
-        <article className="grid w-full justify-between sm:flex md:grid-cols-1">
+        <article className="grid justify-center sm:justify-between sm:flex md:grid-cols-1">
           <Link
             to="/"
-            className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white mx-auto sm:mx-0"
           >
             <span className="p-1 bg-gradient-to-r from-cyan-500 via-sky-500 to-blue-500 rounded-lg text-white">
               Dimterion&apos;s
             </span>
             <span>site</span>
           </Link>
-          <aside className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+          <aside className="grid grid-cols-2 mt-6 sm:mt-0 gap-16">
             <div>
-              <Footer.Title title="About" />
+              <Footer.Title className="mb-4" title="Blog" />
               <Footer.LinkGroup col>
-                <Footer.Link
-                  href="http://dimterion.github.io/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Personal Page
-                </Footer.Link>
-                <Footer.Link
-                  href="/about"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Dimterion&apos;s Blog
-                </Footer.Link>
+                <Footer.Link href="/">Home</Footer.Link>
+                <Footer.Link href="/search">Posts</Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title="Follow me" />
+              <Footer.Title className="mb-4" title="Info" />
               <Footer.LinkGroup col>
-                <Footer.Link
-                  href="https://github.com/Dimterion"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub
-                </Footer.Link>
-                <Footer.Link
-                  href="https://medium.com/@dimterion"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Medium
-                </Footer.Link>
-              </Footer.LinkGroup>
-            </div>
-            <div>
-              <Footer.Title title="Legal" />
-              <Footer.LinkGroup col>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+                <Footer.Link href="/about">About</Footer.Link>
+                <Footer.Link href="/projects">Projects</Footer.Link>
               </Footer.LinkGroup>
             </div>
           </aside>
         </article>
-        <Footer.Divider />
-        <article className="w-full sm:flex sm:items-center sm:justify-between">
+        <hr className="my-4 dark:border-gray-500" />
+        <article className="w-full flex flex-col-reverse sm:flex-row items-center sm:justify-between gap-4">
           <Footer.Copyright
             href="https://github.com/Dimterion/mern-blog"
-            by="Dimterion's blog"
+            by="Dimterion"
             year={new Date().getFullYear()}
           />
-          <aside className="flex gap-6 mt-4 sm:mt-0 sm:justify-center">
+          <aside className="flex gap-6 sm:justify-center">
             <Footer.Icon
               href="https://www.linkedin.com/in/dmitrii-p/"
               target="_blank"
