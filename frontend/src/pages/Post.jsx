5d647290a6e9c4f23418879af0868a776e6838fb
Dmitrii <dimterion59@gmail.com>
2024-03-27 11:07:37 +0100

    adjust post page elements

--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -74,23 +74,23 @@ export default function Post() {
 
   return (
     <main className="p-3 flex flex-col max-w-7xl mx-auto min-h-screen">
-      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+      <h1 className="text-3xl p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
         {post && post.title}
       </h1>
       <Link
         to={`/search?category=${post && post.category}`}
-        className="self-center mt-5"
+        className="self-center my-2"
       >
-        <Button color="gray" pill size="xs">
+        <Button color="gray" pill size="xs" className="pb-1">
           {post && post.category}
         </Button>
       </Link>
       <img
         src={post && post.image}
         alt={post && post.title}
-        className="mt-10 p-3 max-h-[600px] w-full object-cover"
+        className="my-4 max-h-[400px] w-full object-cover rounded-md"
       />
-      <aside className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs">
+      <aside className="flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-sm">
         <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
         <span className="italic">
           {post && (post.content.length / 1000).toFixed(0) > 0
@@ -109,10 +109,10 @@ export default function Post() {
       <CommentSection postId={post._id} />
       <section className="flex flex-col justify-center items-center mb-5">
         <h1 className="text-xl font-semibold mt-5">Recent articles</h1>
-        <article className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 mt-5 justify-center">
+        <aside className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 mt-5 justify-center">
           {recentPosts &&
             recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
-        </article>
+        </aside>
       </section>
     </main>
   );
