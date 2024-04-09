5ae73cc1ae35f10e7f2f6b318cd6c7a16b730823
Dmitrii <dimterion59@gmail.com>
2024-04-09 12:17:27 +0200

    adjust about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,18 +1,21 @@
+import CallToAction from "../components/CallToAction";
+
 export default function About() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <section className="max-w-2xl mx-auto p-3 text-center">
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <section className="max-w-2xl mx-auto mb-8 p-3 text-center">
         <article>
           <h1 className="text-3xl font-semibold text-center my-7">
             About Dimterion&apos;s Blog
           </h1>
-          <section className="text-gray-500 flex flex-col gap-6">
+          <section className="text-gray-500 dark:text-gray-400 flex flex-col gap-6">
             <p>Information about the blog.</p>
             <p>Additional information about the blog.</p>
             <p>More information about the blog.</p>
           </section>
         </article>
       </section>
+      <CallToAction />
     </main>
   );
 }
