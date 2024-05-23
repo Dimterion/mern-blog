560fcd6c3dc05ee5a2645bc056c439137f786811
Dmitrii <dimterion59@gmail.com>
2024-05-23 10:00:42 +0200

    adjust comment comp elements

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import PropTypes from "prop-types";
-import { Button, Textarea } from "flowbite-react";
+import { Textarea } from "flowbite-react";
 import { FaThumbsUp } from "react-icons/fa";
 import moment from "moment";
 
@@ -55,8 +55,8 @@ export default function Comment({ comment, onLike, onEdit, onDelete }) {
   };
 
   return (
-    <section className="flex p-4 border-b dark:border-gray-600 text-sm">
-      <section className="flex-shrink-0 mr-3">
+    <section className="flex p-1 border-b dark:border-gray-600 text-sm">
+      <section className="flex-shrink-0 mr-1">
         <img
           className="size-10 rounded-full bg-gray-200"
           src={user.profilePicture}
@@ -75,28 +75,23 @@ export default function Comment({ comment, onLike, onEdit, onDelete }) {
         {isEditing ? (
           <>
             <Textarea
-              className="mb-2"
+              className="mb-4"
               value={editedContent}
               onChange={(e) => setEditedContent(e.target.value)}
             />
-            <article className="flex justify-end gap-2 text-xs">
-              <Button
-                type="button"
-                size="sm"
-                gradientDuoTone="purpleToBlue"
+            <article className="flex justify-end gap-2 text-xs mb-4">
+              <button
+                className="w-24 border-2 border-sky-500 font-semibold text-sky-500 hover:text-gray-100 hover:bg-sky-500 rounded-lg px-4 py-1 uppercase text-sm"
                 onClick={handleSave}
               >
                 Save
-              </Button>
-              <Button
-                type="button"
-                size="sm"
-                gradientDuoTone="purpleToBlue"
-                outline
+              </button>
+              <button
+                className="w-24 border-2 border-sky-500 font-semibold text-sky-500 hover:text-gray-100 hover:bg-sky-500 rounded-lg px-4 py-1 uppercase text-sm"
                 onClick={() => setIsEditing(false)}
               >
                 Cancel
-              </Button>
+              </button>
             </article>
           </>
         ) : (
@@ -104,7 +99,7 @@ export default function Comment({ comment, onLike, onEdit, onDelete }) {
             <p className="text-gray-600 dark:text-gray-400 pb-2 break-all">
               {comment.content}
             </p>
-            <article className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+            <article className="flex items-center pt-2 text-xs border-t mb-2 dark:border-gray-700 max-w-fit gap-2">
               <button
                 type="button"
                 onClick={() => onLike(comment._id)}
