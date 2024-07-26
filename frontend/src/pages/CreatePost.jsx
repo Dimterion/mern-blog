58a50121f8d44d4719605a51967ebfead534a8f0
Dmitrii <dimterion59@gmail.com>
2024-07-26 12:45:21 +0200

    adjust QuillEditor comp for CreatePost page

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,3 +1,4 @@
+// import { useRef } from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Alert, FileInput, Select, TextInput } from "flowbite-react";
@@ -12,8 +13,10 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+// import QuillEditor from "../components/QuillEditor";
 
 export default function CreatePost() {
+  // const quillRef = useRef();
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
@@ -167,6 +170,13 @@ export default function CreatePost() {
             setFormData({ ...formData, content: value });
           }}
         />
+        {/* <QuillEditor
+          ref={quillRef}
+          onTextChange={() => {
+            const htmlContent = quillRef.current.root.innerHTML;
+            setFormData({ ...formData, content: htmlContent });
+          }}
+        /> */}
         <button
           type="submit"
           className="rounded-lg border-2 border-sky-500 px-4 py-2 font-semibold text-sky-500 hover:bg-sky-500 hover:text-gray-100"
