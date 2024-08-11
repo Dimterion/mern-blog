78fa0b780ee6a974ed4d422a6f225e4b0d5d01b5
Dmitrii <dimterion59@gmail.com>
2024-08-11 11:55:54 +0200

    adjust signup rules

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -17,6 +17,32 @@ export const signup = async (req, res, next) => {
     next(errorHandler(400, "All fields are required."));
   }
 
+  if (req.body.password.length < 6) {
+    return next(
+      errorHandler(400, "Password must be at least 6 characters long."),
+    );
+  }
+
+  if (req.body.username.length < 3 || req.body.username.length > 20) {
+    return next(
+      errorHandler(400, "Username must be between 3 and 20 characters long."),
+    );
+  }
+
+  if (req.body.username.includes(" ")) {
+    return next(errorHandler(400, "Username cannot contain spaces."));
+  }
+
+  if (req.body.username !== req.body.username.toLowerCase()) {
+    return next(errorHandler(400, "Username must be lowercase."));
+  }
+
+  if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+    return next(
+      errorHandler(400, "Username can only contain letters and numbers."),
+    );
+  }
+
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
 
